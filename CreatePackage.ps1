1a35726a2f00a0af565b8cf52fb9024bfe7bc094
Chris Gray <cgray@icsfl.com>
2019-08-27 11:44:39 -0400

    Added create package script

---
+++ CreatePackage.ps1
@@ -0,0 +1,1 @@
+Compress-Archive -Path .\*.html, .\*.js, .\*.gif, .\*.json, .\*icon.png, .\promotional.png, .\*screenshot.png -CompressionLevel Optimal -DestinationPath .\TogglToOpenAir-ChromeExtension.Zip
