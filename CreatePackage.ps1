dc61384371a2df4e4c8e7f48d838274d06b494de
Chris Gray <cgray@icsfl.com>
2019-09-22 21:47:40 -0400

    Add .zip delete to PowerShell script

--- CreatePackage.ps1
+++ CreatePackage.ps1
@@ -1,1 +1,3 @@
+DEL .\TogglToOpenAir-ChromeExtension.Zip
+
 Compress-Archive -Path .\*.html, .\*.js, .\*.gif, .\*.json, .\*icon.png, .\promotional.png, .\*screenshot.png -CompressionLevel Optimal -DestinationPath .\TogglToOpenAir-ChromeExtension.Zip
